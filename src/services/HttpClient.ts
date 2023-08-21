05cf06d5f1db0a9df44f226494138991bd6c3563
Shashank Deshmukh <shashank.deshmukh@qantas.com.au>
2023-08-21 21:32:18 +1000

    Update HttpClient.ts

--- src/services/HttpClient.ts
+++ src/services/HttpClient.ts
@@ -53,3 +53,96 @@ export class HttpClient {
 const httpClientInstance = new HttpClient();
 
 export default httpClientInstance;
+
+
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { HttpClient } from './HttpClient';
+
+describe('HttpClient', () => {
+  let httpClient: HttpClient;
+  let axiosMock: MockAdapter;
+
+  beforeEach(() => {
+    httpClient = new HttpClient();
+    axiosMock = new MockAdapter(httpClient.getInstance());
+  });
+
+  afterEach(() => {
+    axiosMock.restore();
+  });
+
+  // Similar to the GET test, write tests for POST, PUT, and DELETE methods
+
+  it('performs a GET request successfully', async () => {
+    const responseData = { message: 'GET request successful' };
+    axiosMock.onGet('/api/resource').reply(200, responseData);
+
+    const response = await httpClient.get('/api/resource');
+
+    expect(response.data).toEqual(responseData);
+  });
+
+  it('throws an error on GET request failure', async () => {
+    axiosMock.onGet('/api/resource').reply(500, { errorMessage: 'Server error' });
+
+    await expect(httpClient.get('/api/resource')).rejects.toThrowError(
+      'Server error'
+    );
+  });
+
+  
+  it('performs a POST request successfully', async () => {
+    const requestData = { data: 'Test data' };
+    const responseData = { message: 'POST request successful' };
+    axiosMock.onPost('/api/resource').reply(200, responseData);
+
+    const response = await httpClient.post('/api/resource', requestData);
+
+    expect(response.data).toEqual(responseData);
+  });
+
+  it('throws an error on POST request failure', async () => {
+    axiosMock.onPost('/api/resource').reply(500, { errorMessage: 'Server error' });
+
+    await expect(httpClient.post('/api/resource', {})).rejects.toThrowError(
+      'Server error'
+    );
+  });
+
+  it('performs a PUT request successfully', async () => {
+    const requestData = { data: 'Updated data' };
+    const responseData = { message: 'PUT request successful' };
+    axiosMock.onPut('/api/resource').reply(200, responseData);
+
+    const response = await httpClient.put('/api/resource', requestData);
+
+    expect(response.data).toEqual(responseData);
+  });
+
+  it('throws an error on PUT request failure', async () => {
+    axiosMock.onPut('/api/resource').reply(500, { errorMessage: 'Server error' });
+
+    await expect(httpClient.put('/api/resource', {})).rejects.toThrowError(
+      'Server error'
+    );
+  });
+
+  it('performs a DELETE request successfully', async () => {
+    const responseData = { message: 'DELETE request successful' };
+    axiosMock.onDelete('/api/resource').reply(200, responseData);
+
+    const response = await httpClient.delete('/api/resource');
+
+    expect(response.data).toEqual(responseData);
+  });
+
+  it('throws an error on DELETE request failure', async () => {
+    axiosMock.onDelete('/api/resource').reply(500, { errorMessage: 'Server error' });
+
+    await expect(httpClient.delete('/api/resource')).rejects.toThrowError(
+      'Server error'
+    );
+  });
+});
+
